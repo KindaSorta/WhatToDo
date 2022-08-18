eef81fa1380721af87558b6ff57b3b1baea5745a
KindaSorta <gregory.salopek@gmail.com>
2022-08-18 13:31:32 -0500

    Created .NET Maui Project

---
+++ WhatToDo/WhatToDo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WhatToDo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ WhatToDo/WhatToDo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WhatToDo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
